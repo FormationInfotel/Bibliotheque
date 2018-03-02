e0adc434e606028c61238a72f4c1c367cd0dbac7
llev <laurene.level@infotel.com>
2018-03-02 15:01:38 +0100

    dépendances entre couches

---
+++ Biobliotheque-dao/src/main/java/com/infotel/formation/conteneur/ConteneurSpringFullJava.java
@@ -0,0 +1,81 @@
+package com.infotel.formation.conteneur;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource({ "classpath:persistence-mysql.properties" })
+@ComponentScan({ "com.formation" })
+public class ConteneurSpringFullJava {
+
+	@Autowired
+	private Environment env;
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		System.out.println("debut session");
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(restDataSource());
+		sessionFactory.setPackagesToScan(new String[] { "com.formation" });
+		sessionFactory.setHibernateProperties(hibernateProperties());
+		System.out.println("fin session");
+		return sessionFactory;
+
+	}
+
+	@Bean
+	public DataSource restDataSource() {
+		System.out.println("debut datasource");
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.user"));
+		dataSource.setPassword(env.getProperty("jdbc.pass"));
+		System.out.println("fin datasource");
+		return dataSource;
+	}
+
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		System.out.println("debut transaction manager");
+		HibernateTransactionManager txManager = new HibernateTransactionManager();
+		txManager.setSessionFactory(sessionFactory);
+		System.out.println("fin transaction manager");
+
+		return txManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	Properties hibernateProperties() {
+		return new Properties() {
+			private static final long serialVersionUID = 1L;
+
+			{
+				setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+				setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+				setProperty("hibernate.globally_quoted_identifiers", "true");
+			}
+		};
+	}
+
+}
---
+++ Biobliotheque-dao/src/test/java/com/infotel/formation/main/MainTest.java
@@ -0,0 +1,9 @@
+package com.infotel.formation.main;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+
+	}
+
+}
