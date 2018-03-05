6684fcb990d1e4802527b115fa52b15ff122c542
Yoann <yoann.cochennec@infotel.com>
2018-03-05 10:23:16 +0100

    test

--- Bibliotheque-service/src/test/java/com/infotel/formation/main/MainTest.java
+++ Bibliotheque-service/src/test/java/com/infotel/formation/main/MainTest.java
@@ -1,11 +1,13 @@
 package com.infotel.formation.main;
 
+import com.infotel.formation.entity.Author;
+
 public class MainTest {
 
 	public static void main(String[] args) {
 
 
-		// Author auteur = new Author("COCHENNEC", "YOANN");
+		 Author auteur = new Author("COCHENNEC", "YOANN");
 
 	}
 
