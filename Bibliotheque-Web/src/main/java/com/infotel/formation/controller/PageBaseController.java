56dccef4f2e62e50a4c6b4cc2d9401c5180aa89e
llev <laurene.level@infotel.com>
2018-03-07 11:13:03 +0100

    page base complète avec Header et footer

--- Bibliotheque-Web/src/main/java/com/infotel/formation/controller/PageBaseController.java
+++ Bibliotheque-Web/src/main/java/com/infotel/formation/controller/PageBaseController.java
@@ -1,16 +1,14 @@
 package com.infotel.formation.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageBaseController {
 	
 	@RequestMapping("/")
-	public String welcome(Model model) {
-		model.addAttribute("greeting", "Welcome to Web Store");
-		return "welcome";
+	public String getHeader() {
+		return "pageBase";
 	}
 
 }
