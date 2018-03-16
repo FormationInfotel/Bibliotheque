2a2bb596175910ac22a1a3c92e918dea33878892
Yoann <yoann.cochennec@infotel.com>
2018-03-16 11:12:20 +0100

    ajout memberDTO

---
+++ Bibliotheque-Web/src/main/java/com/infotel/formation/DTO/MemberDTO.java
@@ -0,0 +1,96 @@
+package com.infotel.formation.DTO;
+
+import java.io.Serializable;
+
+public class MemberDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+		
+		
+		
+		
+		
+	private long member_id;
+	private String member_lastname;
+	private String member_firstname;
+	private String member_email;
+	private String member_address;
+	private String member_password;
+	
+	private long member_LibraryId;
+
+	public MemberDTO() {
+		super();
+	}
+	
+	
+
+	public MemberDTO(long member_id, String member_lastname, String member_firstname, String member_email,
+			String member_address, String member_password, long member_LibraryId) {
+		super();
+		this.member_id = member_id;
+		this.member_lastname = member_lastname;
+		this.member_firstname = member_firstname;
+		this.member_email = member_email;
+		this.member_address = member_address;
+		this.member_password = member_password;
+		this.member_LibraryId = member_LibraryId;
+	}
+
+
+
+	public long getMember_id() {
+		return member_id;
+	}
+
+	
+	public String getMember_lastname() {
+		return member_lastname;
+	}
+
+	public void setMember_lastname(String member_lastname) {
+		this.member_lastname = member_lastname;
+	}
+
+	public String getMember_firstname() {
+		return member_firstname;
+	}
+
+	public void setMember_firstname(String member_firstname) {
+		this.member_firstname = member_firstname;
+	}
+
+	public String getMember_email() {
+		return member_email;
+	}
+
+	public void setMember_email(String member_email) {
+		this.member_email = member_email;
+	}
+
+	public String getMember_address() {
+		return member_address;
+	}
+
+	public void setMember_address(String member_address) {
+		this.member_address = member_address;
+	}
+
+	public String getMember_password() {
+		return member_password;
+	}
+
+	public void setMember_password(String member_password) {
+		this.member_password = member_password;
+	}
+
+	public long getMember_LibraryId() {
+		return member_LibraryId;
+	}
+
+	public void setMember_LibraryId(long member_LibraryId) {
+		this.member_LibraryId = member_LibraryId;
+	}
+	
+	
+
+}
